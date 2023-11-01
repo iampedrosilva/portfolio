c579095a76d7788a6a0bc910aaa1c9a22cce1f53
zwindowsgamer <83848884+zwindowsgamer@users.noreply.github.com>
2023-11-01 14:12:36 -0300

    Add files via upload
    Atualização de link do instagram

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -95,7 +95,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/iampedro.png"
+                  href="https://www.instagram.com/iampedrosilva.js"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
