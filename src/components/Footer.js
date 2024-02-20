6747aa3165c9a3c5d34ee7bbd6edff324efd6f80
iampedrosilva <83848884+iampedrosilva@users.noreply.github.com>
2024-02-20 08:05:11 -0300

    Update Footer.js
    Atualização de link github

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,7 +23,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/zwindowsgamer"
+                href="https://github.com/iampedrosilva"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
